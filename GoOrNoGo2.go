b0e4dff6e1555e4330a81e0b480316756ef9eb9e
xlstc110 <59573955+xlstc110@users.noreply.github.com>
2020-01-16 10:38:40 -0600

    Version2 with random array numbers and other functions

--- GoOrNoGo2.go
+++ GoOrNoGo2.go
@@ -1,36 +1,40 @@
 package main
 
-import "fmt"
-
+import (
+	"math/rand"
+	"fmt"
+	"time"
+)
 
 func main(){
 	var x int
 
-	fmt.Println(" ")
-	fmt.Println("Welcome to the Go Or No Go, what would you like to do?")
-	fmt.Println(" ")
-	fmt.Println("1: Play go or no go\t 2: Check the scoreboard")
+	fmt.Println("\nWelcome to the Go Or No Go, what would you like to do?")
+	fmt.Println("\n1: Play go or no go\t 2: Check the scoreboard\n")
 	fmt.Scanln(&x)
 
 	if x == 1{
-	
-		fmt.Println(" ")
 
 		box := []string{
 		"box 1","box 2","box 3","box 4","box 5","box 6","box 7","box 8","box 9","box 10",
 		}
-		fmt.Println(box)
-		fmt.Println(" ")
+		fmt.Println(box,"\n")
+
+		//generate a random nonrepeated array to mix the order the box value.
+		rand.Seed(time.Now().UnixNano())
+		order := rand.Perm(10)
+		
+		//Prize list of remaining box value
+		Prize := []int {1000,2500,5000,10000,20000,30000,45000,60000,80000,100000,}
 	
 		//make map dictionary for box
 		Value := make(map[string]int)
 
-		//base value of prize pool
-		ValueBase := 2
+
 
 		//giving value to the boxes
 		for i := 0; i <len(box); i++{
-			Value[box[i]] = ValueBase<<i
+			Value[box[i]] = Prize[order[i]]
 		}
 		//fmt.Println(Value) cheat sheet
 		//user's selection of boxes
@@ -41,26 +45,22 @@ func main(){
 		fmt.Println("Now you have 10 boxes, please select one box that you would like to keep as your lucky box.")
 		fmt.Scanln(&lucky1)
 		luckybox = Value[box[lucky1-1]]
-		
+
 		//drop boxes that have been selected
 		box[lucky1-1] = " "
-
 		
-
-		fmt.Println(" ")
-
 		for {
-
-			fmt.Println(" ")
-			
 			fmt.Println(box)
 			//start to select the boxes to be dropped
 			var select1 int
-			fmt.Println("please select your next box to be dropped")
+			fmt.Println("\nplease select your next box to be dropped")
 			fmt.Scanln(&select1)
 
 			//drop boxes that have been selected
-			box[select1-1] = " "
+			box[select1-1] = "   "
+
+			//drop boxes that have been selected
+			Prize[select1-1] = 0
 
 			//clear the value of seleted boxes from the prize pool
 			Value[box[select1-1]] = 0
@@ -73,8 +73,8 @@ func main(){
 					remain++
 				}
 			}
-			fmt.Println(" ")
-			fmt.Println("Here is the total remaining boxes: ")
+
+			fmt.Println("\nHere is the remaining boxes: \n")
 			fmt.Println(box)
 
 			//total prize pool
@@ -84,39 +84,51 @@ func main(){
 			for j := 0; j < len(box); j++{
 				pool = pool + Value[box[j]]
 			}
-			fmt.Println("Here is the total prize pool amount: ")
-			fmt.Println(pool)
+	
+			fmt.Println("\nHere is the total prize pool: \n", Prize)
+			
 			//banker's offer
 			offer := pool/(remain+1)
-			fmt.Println("The banker is offering to buy your lucky box, if you take the offer, the game will end and you can go with the price. Or you can reject the offer and continue the game")
-			fmt.Println(offer)
+	
+			fmt.Println("\nThe banker is offering to buy your lucky box, if you take the offer, the game will end and you can go with the price. Or you can reject the offer and continue the game")
+			fmt.Println("\nBanker's offer: ",offer)
 
 			var y int
-			fmt.Println("1: Continue the game\t 2: Take the money and Go")
+			fmt.Println("\n1: Continue the game\t 2: Take the money and Go")
 			fmt.Scanln(&y)
 
 			if remain == 0{
 				fmt.Println("All the remaining boxese are gone, you can now go with you luckybox")
-				fmt.Println("Here is your luckybox's value!")
-				fmt.Println(luckybox)
+				fmt.Println("\nHere is your luckybox's value!: ", luckybox)
 				break
 		
 			} else if y == 1 {
 			
 			} else if y == 2{
-				fmt.Println("Accepted banker's offer, here is the prize you earned: ")
-				fmt.Println(offer)
-				fmt.Println("Here is the prize inside your luckybox")
-				fmt.Println(luckybox)
+				fmt.Println("\nAccepted banker's offer, here is the prize you earned: ",offer)
+				fmt.Println("\nHere is the prize inside your luckybox: ",luckybox)
 				break
-			}
+			}	//else if y == selection pool array {
+				// fmt.Println("Please select the box numbers inside the pool")
+			//}
 		}
-	} else {
+	} else if x == 2{
 			fmt.Println("Ops, the scoreboard is empty for now, bye~")
-		}
-		
-}
+	} else {
+		fmt.Println("Please make a selection between 1 and 2")
+	}
 
+	// func droppedbox(){
+	// 	dropped := []string
 
+	// }
 
+	// func restore(){
+	// 	var r int
+	// 	fmt.Println("Here is your first reborn game, the box you select will be restore to the pool")
+	// 	fmt.Println(boxdropped)
+	// 	fmt.Scanln(&r)
+	//  restore the selected box back to the pool
+	// }
 	
+}
