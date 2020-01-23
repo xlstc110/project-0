8b6260d64df0c1833fa3d9bd7522b9ddfd7c3a64
Robbie <robbie.weeks.350@my.csun.edu>
2020-01-23 08:37:46 -0600

    final work

--- GoOrNoGo2.go
+++ GoOrNoGo2.go
@@ -6,27 +6,36 @@ import (
 	"io/ioutil"
 	"log"
 	"math/rand"
+	"net/http"
 	"os"
 	"strconv"
+	"text/template"
 	"time"
 )
 
 func main() {
+	//create a log file to record process journal.
 	logfile, _ := os.Create("log.txt")
 	log.SetOutput(logfile)
 
 	TimeLimit := flag.Int("TimeLimit", 30, "The time limit of each decision round.")
 	flag.Parse()
 
-	var x int
+	var author string
+	author = os.Getenv("USERNAME")
+
 	log.Println("main(): menu.")
 
+	var x int
+
 	fmt.Println("Welcome to the Go Or No Go, what would you like to do?")
-	fmt.Println("1: Play go or no go\t 2: Check the scoreboard")
+	fmt.Println("1: Play go or no go \t 2: Check the scoreboard")
 	fmt.Scanln(&x)
 
 	if x == 1 {
 		log.Println("Game started: lucky box stage.")
+
+		fmt.Println(author + ": Do you know? Number 6 and 8 means luck and fortune in Chinese.\n")
 
 		box := []string{
 			"box 1", "box 2", "box 3", "box 4", "box 5", "box 6", "box 7", "box 8", "box 9", "box 10",
@@ -37,13 +46,13 @@ func main() {
 		rand.Seed(time.Now().UnixNano())
 		order := rand.Perm(10)
 
-		//Prize list of remaining box value
+		//Prize pool slice
 		Prize := []int{1000, 2500, 5000, 10000, 20000, 30000, 45000, 60000, 80000, 100000}
 
-		//make map dictionary for box
+		//make map dictionary that use box as key and prize as value.
 		Value := make(map[string]int)
 
-		//giving value to the boxes
+		//Assign prize into map with a ramdom order
 		for i := 0; i < len(box); i++ {
 			Value[box[i]] = Prize[order[i]]
 		}
@@ -52,15 +61,15 @@ func main() {
 		var lucky1 int
 		var luckybox int
 
-		//select user's lucky box and keep the value.
+		//select user's lucky box and keep the value into a variable.
 		fmt.Println("Now you have 10 boxes, please select one box that you would like to keep as your lucky box.")
 		fmt.Scanln(&lucky1)
 		luckybox = Value[box[lucky1-1]]
 
-		//drop box from stage that have been selected as luckybox
+		//drop box from stage that has been selected as luckybox
 		box[lucky1-1] = " "
 
-		//create a slice to restore all the box that have been selected
+		//create a slice to record all the boxes that have been selected
 		picked := []int{}
 		picked = append(picked, lucky1)
 
@@ -147,24 +156,27 @@ func main() {
 				case decision := <-decisionCh:
 					if remain == 1 {
 						boxGone(luckybox, decision, offer)
+						webPage()
 						return
 					} else if decision == 1 {
 						break
 					} else if decision == 2 {
 						acceptOffer(luckybox, decision, offer)
+						webPage()
 						return
 					}
 
-				//If the player consider over 30 seconds, the game is over and player leave with the luckybox
+				//The player consider over 30 seconds, the game is over and player leave with the luckybox
 				//Receive time out signal from roundTime channel first.
 				case <-roundTime.C:
 					timesUp(luckybox, offer)
+					webPage()
 					return
-
 				}
 
 			}
 		}
+
 	} else if x == 2 {
 		log.Println("Score checking: player choose to check score.")
 
@@ -178,15 +190,47 @@ func main() {
 
 }
 
+//Server alive for score checking.
+func webPage() {
+	http.HandleFunc("/", mainPageHandler)
+	http.HandleFunc("/result", resultHandler)
+	http.ListenAndServe(":8080", nil)
+}
+
+//read content and return *template.Template
+var templates = template.Must(template.ParseFiles("mainpage.html", "result.html"))
+
+//execute template mainpage.html
+func mainPageHandler(w http.ResponseWriter, r *http.Request) {
+
+	templates.ExecuteTemplate(w, "mainpage.html", nil)
+}
+
+//handle request and execute template result.html
+func resultHandler(w http.ResponseWriter, r *http.Request) {
+	name := r.FormValue("name")
+	score, err := load(name)
+	if err != nil {
+		score1 := &player{Name: name, Score: []byte("No record for this player")}
+		score = score1
+	}
+
+	templates.ExecuteTemplate(w, "result.html", score)
+}
+
+//Player did not make a decision in 30 seconds, box stage is over.
+//Show luckybox then create a default decision variable and call record func()
 func timesUp(luckybox int, offer int) {
-	log.Println("Game phrase: player's time up, game over and open the lucky box.")
+	log.Println("Game phrase: player's time up, game over and open the lucky box as final prize.")
 
 	fmt.Println("Ops, your decisioin time ran out, you are now leaving with your lucky box")
 	luckyBox(luckybox)
-	decision := 1
+	decision := 0
 	record(decision, offer, luckybox)
 }
 
+//Player accepted offer, box stage is over
+//Show luckybox then call record func()
 func acceptOffer(luckybox int, decision int, offer int) {
 	log.Println("Game phrase: player accepted the offer, game over and open the lucky box.")
 
@@ -195,6 +239,8 @@ func acceptOffer(luckybox int, decision int, offer int) {
 	record(decision, offer, luckybox)
 }
 
+//All boxes are picked, box stage is over
+//Show luckybox then call record func()
 func boxGone(luckybox int, decision int, offer int) {
 	log.Println("Game phrase: all the boxes on stage is gone, game over and open the lucky box.")
 
@@ -208,7 +254,7 @@ func luckyBox(luckybox int) {
 	fmt.Println("Here is the prize inside your luckybox: ", luckybox)
 }
 
-//func find will loop through the slice and check if the number has previously picked and return a boolean result.
+//func find loop through the slice and check if the number has previously picked and return a boolean result.
 func find(drop []int, select1 int) bool {
 
 	for _, picked := range drop {
@@ -246,6 +292,7 @@ func record(decision int, offer int, luckybox int) {
 			player1.save()
 		}
 		fmt.Println("You nick name and score has recorded to the file!")
+		fmt.Println("You can check your or other's record on the site http://localhost:8080/ or in main menu!")
 
 	} else {
 		log.Println("Record stage: player stay annoymous.")
@@ -253,6 +300,7 @@ func record(decision int, offer int, luckybox int) {
 	}
 }
 
+//func save create a txt file that has player's name and score
 func (p *player) save() error {
 	filename := p.Name + ".txt"
 	return ioutil.WriteFile(filename, p.Score, 0600)
